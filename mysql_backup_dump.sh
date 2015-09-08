ae588f3aeeb5d9a665e00467eac1fc7695112b45
Domonkos Zsolt <zsdomonko@me.com>
2015-09-08 12:50:18 +0200

    inno

--- mysql_backup_dump.sh
+++ mysql_backup_dump.sh
@@ -7,17 +7,16 @@ echo "Config for the backup destination_folder: $dest" >&2
 
 timestamp=$(date +"%F")
 
-mkdir -p $dest
+mkdir -p $dest/MYSQL_DUMP
 
 #MYSQL DUMP SESSION
 
 databases=`mysql --user=$mysql_user -p$mysql_passwd -e "SHOW DATABASES;"`
 
 for db in $databases; do
-  mysqldump --opt --routines --triggers --events --skip-lock-tables --force --user=$mysql_user --password=$mysql_passwd --databases $db | gzip > "$dest/$db-$timestamp.gz"
+  mysqldump --opt --routines --triggers --events --skip-lock-tables --force --user=$mysql_user --password=$mysql_passwd --databases $db | gzip > "$dest/MYSQL_DUMP/$db-$timestamp.gz"
 done
 
 #INNODB SESSION
-mkdir -p $dest/FULL
-innobackupex --user=$mysql_user --password=$mysql_passwd $dest/FULL
-zip -r full-$timestamp.zip $dest/FULL/
+mkdir -p $dest/MYSQL_INNOBACKUP
+innobackupex --stream=tar --user=$mysql_user --password=$mysql_passwd --defaults-file=/etc/mysql/my.cnf ./ | gzip -c -9 > $dest/MYSQL_INNOBACKUP/full.$timestamp.tar.gz
