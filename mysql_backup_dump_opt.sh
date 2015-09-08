152f6c2e77b6d4108269cfbb3376495bec296966
Domonkos Zsolt <zsdomonko@me.com>
2015-09-08 13:20:04 +0200

    comment

--- mysql_backup_dump_opt.sh
+++ mysql_backup_dump_opt.sh
@@ -14,7 +14,7 @@ timestamp=$(date +"%F")
 #databases=`mysql --user=$mysql_user -p$mysql_passwd -e "SHOW DATABASES;"`
 
 #for db in $databases; do
-  mysqldump --opt --routines --triggers --events --skip-lock-tables --force --user=$mysql_user --password=$mysql_passwd --databases $db | gzip > "$dest/MYSQL_DUMP/$db-$timestamp.gz"
+  #mysqldump --opt --routines --triggers --events --skip-lock-tables --force --user=$mysql_user --password=$mysql_passwd --databases $db | gzip > "$dest/MYSQL_DUMP/$db-$timestamp.gz"
 #done
 
 #INNODB SESSION
