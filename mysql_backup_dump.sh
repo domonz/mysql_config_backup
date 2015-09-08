e78700b37fd0ebd90d2cc8070762b4c4c51ef8f6
Domonkos Zsolt <zsdomonko@me.com>
2015-09-08 12:12:16 +0200

    my

--- mysql_backup_dump.sh
+++ mysql_backup_dump.sh
@@ -11,7 +11,7 @@ mkdir -p $dest
 
 #MYSQL DUMP SESSION
 
-databases=`$mysql --user=$mysql_user --password=$mysql_passwd -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
+databases=`mysql --user=$mysql_user --password=$mysql_passwd -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
 
 for db in $databases; do
   mysqldump --opt --routines --triggers --events --skip-lock-tables --force --user=$mysql_user --password=$mysql_passwd --databases $db | gzip > "$dest/$db-$timestamp.gz"
