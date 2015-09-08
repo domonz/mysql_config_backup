2bbc4228eb1edd5befaab1177ebd4d144e5d8a3b
Domonkos Zsolt <zsdomonko@me.com>
2015-09-08 11:33:53 +0200

    MYSQLDUMP SESSION

--- mysql_backup_dump.sh
+++ mysql_backup_dump.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 echo "Reading config...." >&2
 source ./config.cfg
-echo "Config for the backup user: $user" >&2
-echo "Config for the backup password: $passwd" >&2
+echo "Config for the backup user: $mysql_user" >&2
+echo "Config for the backup password: $mysql_passwd" >&2
 echo "Config for the backup destination_folder: $dest" >&2
+
+timestamp=$(date +"%F")
+
+mkdir -p $dest
+
+#MYSQL DUMP SESSION
+
+databases=`$mysql --user=$mysql_user -p$mysql_passwd -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
+
+for db in $databases; do
+  mysqldump --opt --routines --triggers --events --skip-lock-tables --force --user=$mysql_user -p$mysql_passwd --databases $db | gzip > "$dest/$db-$timestamp.gz"
+done
+
