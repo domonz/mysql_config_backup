f90da4838943a950594a7e4e0aa59f5a129c9f18
Domonkos Zsolt <zsdomonko@me.com>
2015-09-08 13:29:11 +0200

    cahneg opt

--- mysql_backup_dump_opt.sh
+++ mysql_backup_dump_opt.sh
@@ -22,32 +22,17 @@ timestamp=$(date +"%F")
 #innobackupex --stream=tar --user=$mysql_user --password=$mysql_passwd --defaults-file=/etc/mysql/my.cnf ./ | gzip -c -9 > $dest/MYSQL_INNOBACKUP/full.$timestamp.tar.gz
 
 
-echo "OPTIND starts at $OPTIND"
-while getopts ":dump:" optname
-  do
-    case "$optname" in
-      "dump")
-        mkdir -p $dest/MYSQL_DUMP
+while getopts ":a" opt; do
+  case $opt in
+    a)
+      mkdir -p $dest/MYSQL_DUMP
         databases=`mysql --user=$mysql_user -p$mysql_passwd -e "SHOW DATABASES;"`
 
 			for db in $databases; do
   				mysqldump --opt --routines --triggers --events --skip-lock-tables --force --user=$mysql_user --password=$mysql_passwd --databases $db | gzip > "$dest/MYSQL_DUMP/$db-$timestamp.gz"
 			done
-        ;;
-      "innodb")
-        mkdir -p $dest/MYSQL_INNOBACKUP
-		innobackupex --stream=tar --user=$mysql_user --password=$mysql_passwd --defaults-file=/etc/mysql/my.cnf ./ | gzip -c -9 > $dest/MYSQL_INNOBACKUP/full.$timestamp.tar.gz
-        ;;
-      "?")
-        echo "Unknown option $OPTARG"
-        ;;
-      ":")
-        echo "No argument value for option $OPTARG"
-        ;;
-      *)
-      # Should not occur
-        echo "Unknown error while processing options"
-        ;;
-    esac
-    echo "OPTIND is now $OPTIND"
-  done
+    ;;
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
