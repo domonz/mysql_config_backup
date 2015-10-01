3bc145bb61df98193187be98b0da19f3ad22d9d5
Domonkos Zsolt <zsdomonko@me.com>
2015-10-01 12:36:46 +0200

    host

--- mysql_backup_dump_opt.sh
+++ mysql_backup_dump_opt.sh
@@ -4,6 +4,7 @@ source ./config.cfg
 echo "Config for the backup user: $mysql_user" >&2
 echo "Config for the backup password: $mysql_passwd" >&2
 echo "Config for the backup destination_folder: $dest" >&2
+echo "Config for the backup mysql_host: $mysql_host" >&2
 
 timestamp=$(date +"%F")
 
@@ -29,7 +30,7 @@ while getopts ":di" opt; do
         databases=`mysql --user=$mysql_user -p$mysql_passwd -e "SHOW DATABASES;"`
 
 			for db in $databases; do
-  				mysqldump --opt --routines --triggers --events --skip-lock-tables --force --user=$mysql_user --password=$mysql_passwd --databases $db | gzip > "$dest/MYSQL_DUMP/$db-$timestamp.gz"
+  				mysqldump --opt --routines --triggers --events --skip-lock-tables --force --host=$mysql_host--user=$mysql_user --password=$mysql_passwd --databases $db | gzip > "$dest/MYSQL_DUMP/$db-$timestamp.gz"
 			done
 	cd $dest/MYSQL_DUMP
 	ls -tQ | tail -n+7 | xargs rm
@@ -37,7 +38,7 @@ while getopts ":di" opt; do
     ;;
     i)
      mkdir -p $dest/MYSQL_INNOBACKUP
-	 innobackupex --stream=tar --user=$mysql_user --password=$mysql_passwd --defaults-file=/etc/mysql/my.cnf ./ | gzip -c -9 > $dest/MYSQL_INNOBACKUP/full.$timestamp.tar.gz
+	 innobackupex --stream=tar --host=$mysql_host --user=$mysql_user --password=$mysql_passwd --defaults-file=/etc/mysql/my.cnf ./ | gzip -c -9 > $dest/MYSQL_INNOBACKUP/full.$timestamp.tar.gz
 	 cd $dest/MYSQL_INNOBACKUP
 	 ls -tQ | tail -n+7 | xargs rm
     ;;   
